437101e26d5b5f33b6c3c3a88b96a1ef037ae60e
Mikael Icaro <mikael.ikaro9@gmail.com>
2025-02-26 21:44:42 -0300

    atualização

--- aula02/desconto.py
+++ aula02/desconto.py
@@ -1,2 +1,27 @@
+
+from regras_desconto import *
+
 salario_bruto = float(input("Digite o salário bruto: "))
+
+INSS = calcular_INSS(salario_bruto)               #salario_bruto * 0.11
+VA = calcular_VA(salario_bruto)                   #salario_bruto * 0.06
+VT = calcular_VT(salario_bruto)                   #salario_bruto * 0.05
+#CRECHE = 0.0 
+
+tem_filho = ''
+
+tem_filho = input("Tem filho ? (S) Sim | (N) não: ")
+while tem_filho != "S" and tem_filho != "N":
+    tem_filho = input("Opção inválida. Digite (S) Sim | (N) não: ").upper()
+
+#if tem_filho == "S":
+CRECHE = calcular_CRECHE(salario_bruto, tem_filho)                                     #salario_bruto * 0.15 
+
+salario_liquido = calcular_salario_liquido(salario_bruto, INSS, VA, VT, CRECHE)                  #salario_bruto - (INSS + VA +VT + CRECHE)
+
+print (f"Seu salario é R$ {salario_liquido}") 
+
+print (f"Total do seu desconto é R$ {calcular_descontos(INSS, VA, VT, CRECHE)}")
+
+#f é uma cotenação de string, que permite a inserção de variáveis dentro de uma string
 
---
+++ aula02/regras_desconto.py
@@ -0,0 +1,20 @@
+def calcular_INSS(sal_bruto):
+    return sal_bruto * 0.11 
+
+def calcular_VA(sal_bruto):
+    return sal_bruto * 0.06
+
+def calcular_VT(sal_bruto):
+    return sal_bruto * 0.05
+
+def calcular_CRECHE(sal_bruto, filhos):
+    if filhos == "S":
+     return sal_bruto * 0.15
+    
+    return 0.0
+
+def calcular_salario_liquido(sal_bruto, val_INSS, val_VA, val_VT, val_CRECHE):
+    return sal_bruto - calcular_descontos(val_INSS,val_VA, val_VT, val_CRECHE)                                                                 #(val_INSS + val_VA + val_VT + val_CRECHE)
+
+def calcular_descontos (val_INSS, val_VA, val_VT, val_CRECHE):
+    return val_INSS + val_VA + val_VT + val_CRECHE
