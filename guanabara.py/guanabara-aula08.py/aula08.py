8d107a109930d5c41988330dc7024ab568d374e0
M1k43l1c4r0 <mikael.ikaro9@gmail.com>
2025-03-03 18:45:25 +0000

    Movendo pasta

---
+++ guanabara.py/guanabara-aula08.py/aula08.py
@@ -0,0 +1,8 @@
+# Utilizando o módulos 
+# import math
+# ceil = arredonda para cima ex: 6.7 = 7
+# floor = arredonda para baixo  ex: 6.7 = 6
+# trunc = elimina a parte fracionária ex: 6.78547 = 6
+# pow = potência ex: pow(4, 2) = 16
+# sqrt = raiz quadrada ex: sqrt(81) = 9
+# factorial = fatorial ex: factorial(5) = 120
