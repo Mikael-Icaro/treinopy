0f34d05f0b3f2ad97514b63f4be58f18c21f1988
M1k43l1c4r0 <mikael.ikaro9@gmail.com>
2025-03-12 15:08:21 +0000

    limpacahce

--- aula02/regras_desconto.py
+++ aula02/regras_desconto.py
@@ -14,7 +14,7 @@ def calcular_CRECHE(sal_bruto, filhos):
     return 0.0
 
 def calcular_salario_liquido(sal_bruto, val_INSS, val_VA, val_VT, val_CRECHE):
-    return sal_bruto - calcular_descontos(val_INSS,val_VA, val_VT, val_CRECHE)                                                                 #(val_INSS + val_VA + val_VT + val_CRECHE)
+    return sal_bruto - calcular_descontos(val_INSS,val_VA, val_VT, val_CRECHE)                                #(val_INSS + val_VA + val_VT + val_CRECHE)
 
 def calcular_descontos (val_INSS, val_VA, val_VT, val_CRECHE):
     return val_INSS + val_VA + val_VT + val_CRECHE
